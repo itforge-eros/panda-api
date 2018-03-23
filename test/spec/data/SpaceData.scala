2a767d940bd88a8036561fe0d90d3a7449c45c2f
kavinvin <kavinvin.vin@gmail.com>
2018-03-24 00:51:27 +0700

    Refactor SpaceData

--- test/spec/data/SpaceData.scala
+++ test/spec/data/SpaceData.scala
@@ -14,4 +14,16 @@ object SpaceData {
     Space(UUID.fromString("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a15"), "324", Some("I want a pizza"), 10, 1, isReservable = false)
   )
 
+  lazy val INSERT_SPACE_DATA: String =
+    """ INSERT INTO Space
+      | VALUES (
+      |   'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
+      |   'M04',
+      |   'I love pizza',
+      |   80,
+      |   1,
+      |   TRUE
+      | )
+    """.stripMargin
+
 }
