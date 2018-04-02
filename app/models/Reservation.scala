cab44e034dbe47aff6792380d26968cd870dc810
kavinvin <kavinvin.vin@gmail.com>
2018-04-02 10:58:41 +0700

    Add space and client field to Reservation model

--- app/models/Reservation.scala
+++ app/models/Reservation.scala
@@ -3,15 +3,26 @@ package models
 import java.util.{Date, UUID}
 
 import entities.ReservationEntity
-import sangria.macros.derive.GraphQLExclude
+import sangria.macros.derive.{GraphQLExclude, GraphQLField}
 import henkan.convert.Syntax._
+import utils.graphql.GraphqlUtil.AppContext
+
+import scala.language.postfixOps
 
 case class Reservation(id: UUID,
                        date: Date,
                        period: Range,
                        isAttended: Boolean,
                        @GraphQLExclude spaceId: UUID,
-                       @GraphQLExclude clientId: UUID)
+                       @GraphQLExclude clientId: UUID) {
+
+  @GraphQLField
+  def space(ctx: AppContext[Request]): Space = ctx.ctx.spacePersist.find(spaceId) map Space.of get
+
+  @GraphQLField
+  def client(ctx: AppContext[Request]): Member = ctx.ctx.memberPersist.find(clientId) map Member.of get
+
+}
 
 object Reservation {
 
