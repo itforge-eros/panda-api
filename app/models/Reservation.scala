75af80ce61a34cebb08d522fbab9aec728e25dc6
kavinvin <kavinvin.vin@gmail.com>
2018-04-02 10:20:05 +0700

    Add Reservation model

---
+++ app/models/Reservation.scala
@@ -0,0 +1,19 @@
+package models
+
+import java.util.{Date, UUID}
+
+import entities.ReservationEntity
+import sangria.macros.derive.GraphQLExclude
+import henkan.convert.Syntax._
+
+case class Reservation(id: UUID,
+                       date: Date,
+                       period: Range,
+                       isAttended: Boolean,
+                       @GraphQLExclude spaceId: UUID)
+
+object Reservation {
+
+  def of(reservationEntity: ReservationEntity): Reservation = reservationEntity.to[Reservation]()
+
+}
