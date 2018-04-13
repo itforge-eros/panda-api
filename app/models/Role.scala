bc205519bb507f669a414d82219dd4ea2300c835
kavinvin <kavinvin.vin@gmail.com>
2018-04-13 20:17:12 +0800

    Add Role model

---
+++ app/models/Role.scala
@@ -0,0 +1,18 @@
+package models
+
+import java.util.UUID
+
+import entities.RoleEntity
+import henkan.convert.Syntax._
+import sangria.macros.derive.GraphQLExclude
+
+case class Role(id: UUID,
+                name: String,
+                description: Option[String],
+                @GraphQLExclude groupId: UUID)
+
+object Role {
+
+  def of(roleEntity: RoleEntity): Role = roleEntity.to[Role]()
+
+}
--- test/spec/data/SpaceData.scala
+++ test/spec/data/SpaceData.scala
@@ -7,12 +7,12 @@ import entities.SpaceEntity
 object SpaceData {
 
   val spaces: List[SpaceEntity] = List(
-    SpaceEntity(Data.uuid1, "M23", Some("The lock is broken"), Some(90), isAvailable = true, Instant.now()),
-    SpaceEntity(Data.uuid2, "M04", None, Some(90), isAvailable = false, Instant.now()),
-    SpaceEntity(Data.uuid3, "203", None, Some(50), isAvailable = true, Instant.now()),
-    SpaceEntity(Data.uuid4, "207", Some("1112"), Some(50), isAvailable = true, Instant.now()),
-    SpaceEntity(Data.uuid5, "324", Some("I want a pizza"), Some(10), isAvailable = false, Instant.now()),
-    SpaceEntity(Data.uuid6, "324", Some("I want a pizza"), Some(10), isAvailable = false, Instant.now())
+    SpaceEntity(Data.uuid1, "M23", Some("The lock is broken"), Some(90), isAvailable = true, Instant.now(), Data.uuid),
+    SpaceEntity(Data.uuid2, "M04", None, Some(90), isAvailable = false, Instant.now(), Data.uuid),
+    SpaceEntity(Data.uuid3, "203", None, Some(50), isAvailable = true, Instant.now(), Data.uuid),
+    SpaceEntity(Data.uuid4, "207", Some("1112"), Some(50), isAvailable = true, Instant.now(), Data.uuid),
+    SpaceEntity(Data.uuid5, "324", Some("I want a pizza"), Some(10), isAvailable = false, Instant.now(), Data.uuid),
+    SpaceEntity(Data.uuid6, "324", Some("I want a pizza"), Some(10), isAvailable = false, Instant.now(), Data.uuid)
   )
 
   lazy val INSERT_SPACE_DATA: String =
