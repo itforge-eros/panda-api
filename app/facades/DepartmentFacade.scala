ad37af91f7a2ea2b559cee81f6f1d6dcda5787bd
kavinvin <kavinvin.vin@gmail.com>
2018-04-13 16:59:50 +0800

    Add create method to DepartmentFacade

--- app/facades/DepartmentFacade.scala
+++ app/facades/DepartmentFacade.scala
@@ -5,13 +5,22 @@ import java.util.UUID
 import definitions.exceptions.DepartmentException.DepartmentNotFoundException
 import models.Department
 import persists.DepartmentPersist
+import schemas.inputs.DepartmentInput
 
-import scala.util.Try
+import scala.util.{Success, Try}
 
 class DepartmentFacade(departmentPersist: DepartmentPersist) extends BaseFacade {
 
   def find(id: UUID): Try[Department] = ValidateWith() {
     departmentPersist.find(id) toTry DepartmentNotFoundException map Department.of
+  }
+
+  def create(input: DepartmentInput): Try[Department] = ValidateWith() {
+    Success(Department(
+      UUID.randomUUID(),
+      "random name",
+      Some("whatever")
+    ))
   }
 
 }
--- app/schemas/mutations/DepartmentMutation.scala
+++ app/schemas/mutations/DepartmentMutation.scala
@@ -1,22 +1,14 @@
 package schemas.mutations
 
-import java.util.UUID
-
-import models.{BaseModel, Department}
+import models.BaseModel
 import sangria.macros.derive.GraphQLField
 import schemas.inputs.DepartmentInput
-
-import scala.util.Success
 
 trait DepartmentMutation extends BaseModel {
 
   @GraphQLField
   def createDepartment(input: DepartmentInput)(ctx: AppContext[Unit]) = authorize(ctx) { implicit member =>
-    Success(Department(
-      UUID.randomUUID(),
-      "random name",
-      Some("whatever")
-    ))
+    ctx.ctx.departmentFacade.create(input)
   }
 
 }
