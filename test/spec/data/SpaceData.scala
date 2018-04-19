3d105011c30efe7d3d901a06fbdc48dbf60d6aae
kavinvin <kavinvin.vin@gmail.com>
2018-04-19 10:12:55 +0700

    Fix broken test

--- test/spec/data/SpaceData.scala
+++ test/spec/data/SpaceData.scala
@@ -3,16 +3,18 @@ package spec.data
 import java.time.Instant
 
 import entities.SpaceEntity
+import models.enums.SpaceCategory
+import models.enums.SpaceCategory._
 
 object SpaceData {
 
   val spaces: List[SpaceEntity] = List(
-    SpaceEntity(Data.uuid1, "M23", "M23", Some("The lock is broken"), Some(90), isAvailable = true, Instant.now(), Data.uuid),
-    SpaceEntity(Data.uuid2, "M04", "M23", None, Some(90), isAvailable = false, Instant.now(), Data.uuid),
-    SpaceEntity(Data.uuid3, "203", "M23", None, Some(50), isAvailable = true, Instant.now(), Data.uuid),
-    SpaceEntity(Data.uuid4, "207", "M23", Some("1112"), Some(50), isAvailable = true, Instant.now(), Data.uuid),
-    SpaceEntity(Data.uuid5, "324", "M23", Some("I want a pizza"), Some(10), isAvailable = false, Instant.now(), Data.uuid),
-    SpaceEntity(Data.uuid6, "324", "M23", Some("I want a pizza"), Some(10), isAvailable = false, Instant.now(), Data.uuid)
+    SpaceEntity(Data.uuid1, "M23", "M23", Some("The lock is broken"), Classroom.name, Some(90), isAvailable = true, Instant.now(), Data.uuid),
+    SpaceEntity(Data.uuid2, "M04", "M23", None, MeetingRoom.name, Some(90), isAvailable = false, Instant.now(), Data.uuid),
+    SpaceEntity(Data.uuid3, "203", "M23", None, MeetingRoom.name, Some(50), isAvailable = true, Instant.now(), Data.uuid),
+    SpaceEntity(Data.uuid4, "207", "M23", Some("1112"), MeetingRoom.name, Some(50), isAvailable = true, Instant.now(), Data.uuid),
+    SpaceEntity(Data.uuid5, "324", "M23", Some("I want a pizza"), MeetingRoom.name, Some(10), isAvailable = false, Instant.now(), Data.uuid),
+    SpaceEntity(Data.uuid6, "324", "M23", Some("I want a pizza"), MeetingRoom.name, Some(10), isAvailable = false, Instant.now(), Data.uuid)
   )
 
   lazy val INSERT_SPACE_DATA: String =
