1b129a27a24739ac152c7db10755c4f6e3051e9b
kavinvin <kavinvin.vin@gmail.com>
2018-04-20 16:25:22 +0700

    Modify role query to find by id instead of name

--- app/schema/query/RoleQuery.scala
+++ app/schema/query/RoleQuery.scala
@@ -1,13 +1,15 @@
 package schema.query
 
+import java.util.UUID
+
 import models.{BaseModel, Role}
 import sangria.macros.derive.GraphQLField
 
 trait RoleQuery extends BaseModel {
 
   @GraphQLField
-  def role(department: String, name: String)(ctx: AppContext[Unit]): Option[Role] = authorizeOption(ctx) { implicit viewer =>
-    ctx.ctx.roleFacade.findByName(department, name)
+  def role(id: UUID)(ctx: AppContext[Unit]): Option[Role] = authorizeOption(ctx) { implicit viewer =>
+    ctx.ctx.roleFacade.find(id)
   }
 
 }
